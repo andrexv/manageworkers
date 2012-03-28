655219162efdad793cb0adb9305889546020f415
Andres Visbal <andresvisbal@gmail.com>
2012-03-27 19:19:38 -0500

    Adding Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManageWorkers::Application.config.secret_token = '65d4aff5bbb391cbf6d618143876685ccc0c95e6ee7f3f76c10150df56abe31ce63c213030bc5715894f9cd71b3cd1285a943079464d83c75f2211877d682b7f'
